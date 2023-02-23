2f0a55de217307323c80370d4db023739d7f4f20
Giftzwerg02 <benjamin.komar@kabsi.at>
2023-02-23 16:49:43 +0100

    added basic leave-after-song-end

---
+++ src/error.rs
@@ -0,0 +1,15 @@
+use poise::{
+    serenity_prelude::{CreateEmbed, CreateMessage, Timestamp},
+    CreateReply,
+};
+
+pub type Error = Box<dyn std::error::Error + Send + Sync>;
+
+pub fn error_embed<'a>(create: &'a mut CreateEmbed, error: &Error) -> &'a mut CreateEmbed {
+    create.title("Woopsie doodle, something happened owo")
+        .description("An error occured because of (most likely) your incompetence :)")
+        .thumbnail("https://raw.githubusercontent.com/Giftzwerg02/oxo/33856f5c3ad1549de092f7f58a83b05e1b060398/resources/unsafe-ferris-transparent.png")
+        .field("Error", format!("```{:?}```", error), false)
+        .footer(|f| f.text("XOXO"))
+        .timestamp(Timestamp::now())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,53 @@
-use std::path::Path;
+use std::{path::Path, sync::Arc};
 
 use dotenvy::dotenv;
-use poise::{serenity_prelude::{self as serenity, Timestamp, EmbedImage}, FrameworkError};
-use songbird::SerenityInit;
+use poise::{
+    async_trait,
+    serenity_prelude::{
+        self as serenity, CacheHttp, ChannelId, EmbedImage, Http, Message, Mutex, Timestamp,
+    },
+    FrameworkError,
+};
+use songbird::{tracks::Track, Call, Event, EventContext, EventHandler, SerenityInit, TrackEvent};
+mod error;
+use error::{error_embed, Error};
+use tracing::{info, debug, warn, error, trace};
 
 struct Data {} // User data, which is stored and accessible in all command invocations
-type Error = Box<dyn std::error::Error + Send + Sync>;
 type Context<'a> = poise::Context<'a, Data, Error>;
+
+struct LeaveAfterEndEventHandler {
+    handler: Arc<Mutex<Call>>,
+    http: Arc<Http>,
+    channel_id: ChannelId,
+}
+
+#[async_trait]
+impl EventHandler for LeaveAfterEndEventHandler {
+    async fn act(&self, _ctx: &EventContext<'_>) -> Option<Event> {
+        debug!(channel_id = self.channel_id.to_string(), "Triggered leave-after-end-event-handler");
+    
+        let mut handler = self.handler.lock().await;
+        let res = handler.leave().await;
+
+        if let Err(err) = res {
+            self.channel_id
+                .send_message(&self.http, |create| {
+                    create.embed(|e| error_embed(e, &err.into()))
+                })
+                .await;
+        }
+
+        None
+    }
+}
 
 /// Jamming
 #[poise::command(slash_command, prefix_command)]
-async fn play(ctx: Context<'_>, #[description = "URL"] url: String) -> Result<(), Error> {
+async fn play(
+    ctx: Context<'_>,
+    #[description = "URL"] url: String,
+) -> Result<(), Error> {
     let guild = ctx.guild().unwrap();
     let channel_id = guild
         .voice_states
@@ -18,51 +55,88 @@ async fn play(ctx: Context<'_>, #[description = "URL"] url: String) -> Result<()
         .and_then(|vs| vs.channel_id)
         .unwrap();
 
-    let ctx = ctx.serenity_context();
+    let serenity_ctx = ctx.serenity_context();
 
-    let manager = songbird::get(ctx).await.unwrap().clone();
+    let manager = songbird::get(serenity_ctx).await.unwrap().clone();
 
     let (handler, _) = manager.join(guild.id, channel_id).await;
 
-    let mut handler = handler.lock().await;
+    let mut handler_lock = handler.lock().await;
     let source = songbird::ytdl(&url).await?;
 
-    handler.play_source(source);
+    let _ = handler_lock.play_source(source);
+
+    handler_lock.add_global_event(
+        Event::Track(TrackEvent::End),
+        LeaveAfterEndEventHandler {
+            handler: handler.clone(),
+            channel_id: ctx.channel_id(),
+            http: serenity_ctx.http.clone(),
+        },
+    );
+
+    // tracker.add_event(Event::Track(TrackEvent::End), LeaveAfterEndEventHandler {
+    //     handler: handler.clone(),
+    //     poise_ctx: ctx.clone()
+    // });
 
     Ok(())
 }
 
 async fn on_error(error: FrameworkError<'_, Data, Error>) {
     let res = match error {
-        FrameworkError::Setup { error, framework, data_about_bot, ctx } => todo!(),
-        FrameworkError::EventHandler { error, ctx, event, framework } => todo!(),
+        FrameworkError::Setup {
+            error,
+            framework,
+            data_about_bot,
+            ctx,
+        } => todo!(),
+        FrameworkError::EventHandler {
+            error,
+            ctx,
+            event,
+            framework,
+        } => todo!(),
         FrameworkError::Command { error, ctx } => {
-            tracing::error!(error = error, "Unexpected error occured");
+            error!(error = error, "Unexpected error occured");
 
-            ctx.send(|msg| {
-                msg.embed(|e| {
-                    e.title("Woopsie doodle, something happened owo")
-                        .description("An error occured because of (most likely) your incompetence :)")
-                        .thumbnail("https://raw.githubusercontent.com/Giftzwerg02/oxo/33856f5c3ad1549de092f7f58a83b05e1b060398/resources/unsafe-ferris-transparent.png")
-                        .field("Error", format!("```{:?}```", error), false)
-                        .footer(|f| f.text("XOXO"))
-                        .timestamp(Timestamp::now())
-                })
-            }).await
-        },
+            ctx.send(|create| create.embed(|e| error_embed(e, &error)))
+                .await
+        }
         FrameworkError::ArgumentParse { error, input, ctx } => todo!(),
         FrameworkError::CommandStructureMismatch { description, ctx } => todo!(),
-        FrameworkError::CooldownHit { remaining_cooldown, ctx } => todo!(),
-        FrameworkError::MissingBotPermissions { missing_permissions, ctx } => todo!(),
-        FrameworkError::MissingUserPermissions { missing_permissions, ctx } => todo!(),
+        FrameworkError::CooldownHit {
+            remaining_cooldown,
+            ctx,
+        } => todo!(),
+        FrameworkError::MissingBotPermissions {
+            missing_permissions,
+            ctx,
+        } => todo!(),
+        FrameworkError::MissingUserPermissions {
+            missing_permissions,
+            ctx,
+        } => todo!(),
         FrameworkError::NotAnOwner { ctx } => todo!(),
         FrameworkError::GuildOnly { ctx } => todo!(),
         FrameworkError::DmOnly { ctx } => todo!(),
         FrameworkError::NsfwOnly { ctx } => todo!(),
         FrameworkError::CommandCheckFailed { error, ctx } => todo!(),
         FrameworkError::DynamicPrefix { error, ctx, msg } => todo!(),
-        FrameworkError::UnknownCommand { ctx, msg, prefix, msg_content, framework, invocation_data, trigger } => todo!(),
-        FrameworkError::UnknownInteraction { ctx, framework, interaction } => todo!(),
+        FrameworkError::UnknownCommand {
+            ctx,
+            msg,
+            prefix,
+            msg_content,
+            framework,
+            invocation_data,
+            trigger,
+        } => todo!(),
+        FrameworkError::UnknownInteraction {
+            ctx,
+            framework,
+            interaction,
+        } => todo!(),
         _ => todo!(),
     };
 
@@ -82,7 +156,7 @@ async fn main() {
         .options(poise::FrameworkOptions {
             commands: vec![play()],
             on_error: |err| Box::pin(on_error(err)),
-            ..Default::default() 
+            ..Default::default()
         })
         .token(token)
         .intents(serenity::GatewayIntents::non_privileged())
