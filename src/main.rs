b3ec786d4581ae29133e3a1916ca0a6cb91bacc1
Giftzwerg02 <benjamin.komar@kabsi.at>
2023-02-25 23:55:49 +0100

    added now_playing and queue commands

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,38 @@
 use std::{
-    collections::{HashMap, VecDeque},
-    sync::Arc,
+    collections::{HashMap},
+    sync::Arc, time::Duration,
 };
 
 use dotenvy::dotenv;
 use poise::{
     async_trait,
-    serenity_prelude::{self as serenity, ChannelId, GuildId, Http, Mutex, RwLock, Guild},
+    serenity_prelude::{self as serenity, ChannelId, GuildId, Http, Mutex, Colour, Timestamp},
     FrameworkError,
 };
 use songbird::{
-    tracks::{Track, TrackHandle, TrackQueue},
-    Call, Event, EventContext, EventHandler, SerenityInit, TrackEvent, input::Input, create_player,
+    tracks::TrackQueue,
+    Call, Event, EventContext, EventHandler, SerenityInit, TrackEvent,
 };
 mod error;
 use error::{error_embed, log_unexpected_error, Error};
 use tracing::debug;
 
+
 type Context<'a> = poise::Context<'a, State, Error>;
 type CmdRes = Result<(), Error>;
 
+type Queues = Arc<Mutex<HashMap<GuildId, TrackQueue>>>;
+
 #[derive(Debug)]
 struct State {
-    queues: Arc<Mutex<HashMap<GuildId, TrackQueue>>>,
+    queues: Queues,
 }
 
 struct EndEventHandler {
-    handler: Arc<Mutex<Call>>,
     http: Arc<Http>,
     channel_id: ChannelId,
+    queues: Queues,
+    call: Arc<Mutex<Call>>,  
     guild_id: GuildId,
 }
 
@@ -45,12 +49,125 @@ impl EventHandler for EndEventHandler {
         };
 
         // double de-ref LETS GO
-        let [(state, handle)] = **tracks else {
+        let [(_, handle)] = **tracks else {
             return None;
         };
 
+        let fallback_title = "No title found,  no seriously this is not the name of the track - for some reason there just isn't one".to_string();
+        let title = handle.metadata().title.as_ref().unwrap_or(&fallback_title);
+
+        let _ = self.channel_id.say(&self.http, format!("Finished playing `{}` uwu", title))
+            .await;
+
+        let queues = self
+            .queues
+            .lock()
+            .await;
+
+        let queue = queues 
+            .get(&self.guild_id)
+            .unwrap(); 
+
+        if queue.is_empty() {
+            let _ = self.channel_id.say(&self.http, "No more songs to play OwO, guess I'll go...")
+                .await;
+
+            let mut call = self.call.lock().await;
+            let _ = call.leave().await;
+        }
+
         None
     }
+}
+
+
+/// I WANT 'EM ALL - I WANT 'EM NOW
+#[poise::command(slash_command)]
+async fn queue(ctx: Context<'_>) -> CmdRes {
+    let guild = ctx.guild().unwrap();
+    let queues = ctx.data().queues.lock().await;
+    let queue = queues.get(&guild.id)
+        .unwrap();
+
+    let list = queue.current_queue();
+
+    let formatted_list = list
+        .iter()
+        .map(|track| format!("▷ {}", track.metadata().title.clone().unwrap_or("N/A".into())))
+        .collect::<Vec<_>>()
+        .join("\n");
+
+    ctx.send(|create| create.embed(|e| e
+        .title("Current Playlist")
+        .description(format!("{} songs are currently queued", list.len()))
+        .field("Queue", formatted_list, false)
+        .colour(Colour::BLITZ_BLUE)
+        .footer(|f| f.text("XOXO"))
+        .timestamp(Timestamp::now())
+    )).await?;
+
+    Ok(())
+}
+
+/// Who asked?
+#[poise::command(slash_command)]
+async fn now_playing(ctx: Context<'_>) -> CmdRes {
+    let guild = ctx.guild().unwrap();
+    let queues = ctx.data().queues.lock().await;
+    let queue = queues.get(&guild.id)
+        .unwrap();
+
+    let current = queue.current()
+        .unwrap();
+
+
+    let metadata = current.metadata();
+    let track_info = current.get_info().await?;
+
+    let colour = Colour::BLITZ_BLUE; 
+    let title = metadata.title.clone().unwrap_or("N/A".into()); 
+    let author = format!("By: {}", metadata.artist.clone().unwrap_or("N/A".into()));
+
+    let thumbnail = metadata.thumbnail.clone().unwrap_or("https://www.keil.com/support/man/docs/ulinkplus/could_not_load_file.png".into());
+
+    let total_duration = metadata.duration.unwrap_or_default();
+    let already_played = track_info.position;
+
+
+    fn duration_format(duration: &Duration) -> String {
+        let seconds = duration.as_secs();
+        let minutes = seconds / 60;
+        let hours = minutes / 60;
+        let minutes = minutes % 60;
+        let seconds = seconds % 60;
+
+        if hours > 0 {
+            format!("{:02}:{:02}:{:02}", hours, minutes, seconds)
+        } else {
+            format!("{:02}:{:02}", minutes, seconds)
+        }
+    }
+
+    let description = format!("Duration: `{} / {}`", duration_format(&already_played), duration_format(&total_duration)); 
+    let url = metadata.source_url.clone().unwrap_or("https://www.youtube.com/watch?v=dQw4w9WgXcQ".into());
+
+    ctx.send(|create| 
+        create
+            .embed(|e| e
+                .colour(colour)
+                .title(title)
+                .author(|a| a
+                        .name(author)
+                        .icon_url("https://cdn-icons-png.flaticon.com/512/2995/2995101.png"))
+                .thumbnail(thumbnail)
+                .description(description)
+                .url(url)
+                .footer(|f| f.text("XOXO"))
+                .timestamp(Timestamp::now())
+            )
+        ).await?;
+
+    Ok(())
 }
 
 /// Hol' up
@@ -114,10 +231,11 @@ async fn play(ctx: Context<'_>, #[description = "URL"] url: String) -> CmdRes {
     let track = queue.add_source(source, &mut handler_lock);
 
     track.add_event(Event::Track(TrackEvent::End), EndEventHandler {
-        handler: handler.clone(),
         channel_id: ctx.channel_id(),
         http: ctx.serenity_context().http.clone(),
+        call: handler.clone(),
         guild_id: guild.id,
+        queues: ctx.data().queues.clone()
     })?;
 
     Ok(())
@@ -205,7 +323,7 @@ async fn main() {
 
     let framework = poise::Framework::builder()
         .options(poise::FrameworkOptions {
-            commands: vec![play(), pause(), resume(), skip()],
+            commands: vec![play(), pause(), resume(), skip(), now_playing(), queue()],
             on_error: |err| Box::pin(on_error(err)),
             ..Default::default()
         })
